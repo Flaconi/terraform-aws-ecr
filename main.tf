1c630cce4daa08694c03143886ff551912e3b9f8
maartenvanderhoef <maarten@vanderhoef.eu>
2019-03-01 18:39:48 +0100

    Can't have policy yet, will re-implement

--- main.tf
+++ main.tf
@@ -20,29 +20,6 @@ resource "aws_ecr_repository" "default" {
   name  = "${var.use_fullname == "true" ? module.label.id : module.label.name}"
 }
 
-resource "aws_ecr_lifecycle_policy" "default" {
-  count      = "${var.enabled == "true" ? 1 : 0}"
-  repository = "${aws_ecr_repository.default.name}"
-
-  policy = <<EOF
-{
-  "rules": [{
-    "rulePriority": 1,
-    "description": "Rotate images when reach ${var.max_image_count} images stored",
-    "selection": {
-      "tagStatus": "tagged",
-      "tagPrefixList": ["${var.stage}"],
-      "countType": "imageCountMoreThan",
-      "countNumber": ${var.max_image_count}
-    },
-    "action": {
-      "type": "expire"
-    }
-  }]
-}
-EOF
-}
-
 data "aws_iam_policy_document" "empty" {}
 
 data "aws_iam_policy_document" "resource_readonly_access" {
